b13fde91587246ba405f4eced018ea432731e221
StrictDesign <sprng08wq!>
2014-05-02 16:12:51 +0200

    Initial View Controller created
    Created view controllers, models and some storyboards

---
+++ ObjectiveCTrainerApp/ObjectiveCTrainerApp/Menu/MenuModel.m
@@ -0,0 +1,13 @@
+//
+//  MenuModel.m
+//  ObjectiveCTrainerApp
+//
+//  Created by Jimmy Mjällby on 2014-05-02.
+//  Copyright (c) 2014 StrictDesign. All rights reserved.
+//
+
+#import "MenuModel.h"
+
+@implementation MenuModel
+
+@end
